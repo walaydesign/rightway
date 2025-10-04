680f559c1ab120127b5082d5d48246a48f5f45f9
youzhen <yl301114@gmail.com>
2025-10-04 13:07:00 +0100

    桌機版

---
+++ public/js/common.js
@@ -0,0 +1,78 @@
+AOS.init({ startEvent: "load" });
+window.addEventListener("load", AOS.refresh);
+
+// go to top
+$(".btn-top").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 300);
+});
+
+resize();
+$(window).on("resize scroll", function () {
+  resize();
+});
+
+function resize() {
+  // header
+  if ($(window).scrollTop() > 0) {
+    $(".header").addClass("scrolldown");
+  } else {
+    $(".header").removeClass("scrolldown");
+  }
+}
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 6, //一次移動幾個
+  centeredSlides: true, //對準中心
+  loop: true, //無限輪迴
+  direction: "vertical",
+  observeParents: true,
+  observer: true,
+  allowTouchMove: false,
+  thumbs: {
+    swiper: "swiper名稱",
+  },
+  simulateTouch: false, //不能點選
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "progressbar", //型態:進度條
+  },
+  scrollbar: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  keyboard: {
+    enable: true, //可用鍵盤控制
+  },
+  effect: "fade",
+  speed: 1000,
+  fadeEffect: {
+    crossFade: true,
+  },
+  allowTouchMove: false,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
